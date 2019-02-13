546a99aa9b211220f5a8a4bd55af7eb6b1e9c014
WGNSpear <40544275+WGNSpear@users.noreply.github.com>
2019-02-13 19:13:35 +0000

    added template command

---
+++ commands/template.js
@@ -0,0 +1,12 @@
+const Discord =require("discord.js");
+const fs = require("fs");
+//any requirements go here
+module.exports.run = async (bot, message, args) => {
+
+    //Command code goes here
+
+}
+
+module.exports.help = {
+    name: "template" //Must rename this to name of the file
+}
